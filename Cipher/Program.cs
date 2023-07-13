fcfe8ea81cd9782d6462af61b636164fee2cbb4f
morendo <morendo90@gmail.com>
2023-07-13 21:53:44 +0900

    곱셈 암호화 알고리즘 구현

--- Cipher/Program.cs
+++ Cipher/Program.cs
@@ -21,17 +21,23 @@ namespace Cipher
             Console.WriteLine("Caesar 복호화:" + decryptStr);
 
             var encryptRot13Str = ROT13Cipher.Encrypt("HELLO");
-            var decryptRot13Str = ROT13Cipher.Decrypt(encryptStr);
+            var decryptRot13Str = ROT13Cipher.Decrypt(encryptRot13Str);
 
             RandomCipher randomCipher = new RandomCipher();
             Console.WriteLine("ROT13 암호화:" + encryptRot13Str);
             Console.WriteLine("ROT13 복호화:" + decryptRot13Str);
 
             var encryptRandomStr = randomCipher.Encrypt("HELLO");
-            var decryptRandomStr = randomCipher.Decrypt(encryptStr);
+            var decryptRandomStr = randomCipher.Decrypt(encryptRandomStr);
 
             Console.WriteLine("Random 암호화:" + encryptRandomStr);
             Console.WriteLine("Random 복호화:" + decryptRandomStr);
+
+            var encryptMulti = MultiCipher.Encrypt("HELLO", 3);
+            var decryptMulti = MultiCipher.Decrypt(encryptMulti, 3);
+
+            Console.WriteLine("곱셈 암호화:" + encryptMulti);
+            Console.WriteLine("곱셈 복호화:" + decryptMulti);
         }
 
     }
---
+++ Cipher/Substitution Cipher/MultiCipher.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cipher.Substitution_Cipher
+{
+    /// <summary>
+    /// 곱셈 암호
+    /// </summary>
+    public class MultiCipher
+    {
+        public static string Encrypt(string message, int key)
+        {
+            char[] a = message.ToCharArray();
+
+            for (int i = 0; i < a.Length; i++) {
+                if (!char.IsWhiteSpace(a[i])) {
+                    a[i] = (char)('A' + ((a[i] - 'A') * key) % 26);
+
+                }
+            }
+
+            return new string(a);
+        }
+
+        public static string Decrypt(string cipher, int key) {
+            char[] a = cipher.ToCharArray();
+
+            //곱셈 역원 계산
+            int inverseKey = GetMultiplicativeInverse(key);
+
+            for (int i = 0; i < a.Length; i++) {
+                if (!char.IsWhiteSpace(a[i])) {
+                    a[i] = (char)('A' + ((a[i] - 'A') * inverseKey) % 26);
+                }
+            }
+                       
+            return new string(a);
+        }
+
+        private static int GetMultiplicativeInverse(int key) {
+            // 1~25 까지 순차적으로 대입
+        
+            for (int i = 1; i < 26; i++) {
+                if ((key * i) % 26 == 1) {
+                    return i;
+                }    
+            }
+            
+            throw new InvalidOperationException();
+        }
+
+        internal static void HowToTest()
+        {
+            string message = "ATTACK WEST CASTLE".ToUpper();
+            var caesar = new CaesarCipher();
+            string cipher = caesar.Encrypt(message);
+            string plain = caesar.Decrypt(cipher);
+
+            Console.WriteLine($"{message},{cipher},{plain}");
+            Debug.Assert(message == plain);
+
+        }
+
+    }
+}
